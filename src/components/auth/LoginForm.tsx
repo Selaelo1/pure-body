a2dbeab30ac0e08d9f090256c48bc28bf84b309f
Selaelo1 <selaelolanga10@gmail.com>
2024-11-01 11:17:36 +0200

    Update LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { useAuthStore } from "../../stores/authStore";
+import { useAuthStore } from "../../store/authStore";
 import { useNavigate } from "react-router-dom";
 
 const loginSchema = z.object({
