048b3dc9a8ed0c7e7cce84e90f2344981fbcd13f
Selaelo1 <selaelolanga10@gmail.com>
2024-11-02 10:57:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import TrainersPage from "./pages/Trainers";
+import BookingPage from "./pages/Booking";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
@@ -15,6 +17,22 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/trainers"
+          element={
+            <ProtectedRoute>
+              <TrainersPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/book/:trainerId"
+          element={
+            <ProtectedRoute>
+              <BookingPage />
+            </ProtectedRoute>
+          }
+        />
         <Route
           path="/dashboard/*"
           element={
@@ -28,5 +46,3 @@ function App() {
     </BrowserRouter>
   );
 }
-
-export default App;
